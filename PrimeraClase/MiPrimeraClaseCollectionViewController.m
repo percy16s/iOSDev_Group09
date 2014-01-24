0e765755efce1aefabe8376e94a994a79dde3764
eldervaz <eldervaz@gmail.com>
2014-01-24 13:01:50 -0500

    23 enero

---
+++ PrimeraClase/MiPrimeraClaseCollectionViewController.m
@@ -0,0 +1,38 @@
+//
+//  MiPrimeraClaseCollectionViewController.m
+//  PrimeraClase
+//
+//  Created by Franti Saúl Huamán Mera on 1/23/14.
+//  Copyright (c) 2014 Franti Saúl Huamán Mera. All rights reserved.
+//
+
+#import "MiPrimeraClaseCollectionViewController.h"
+
+@interface MiPrimeraClaseCollectionViewController ()
+
+@end
+
+@implementation MiPrimeraClaseCollectionViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ PrimeraClase/MiPrimeraClaseParaUnTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  MiPrimeraClaseParaUnTableViewController.h
+//  PrimeraClase
+//
+//  Created by Franti Saúl Huamán Mera on 1/23/14.
+//  Copyright (c) 2014 Franti Saúl Huamán Mera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MiPrimeraClaseParaUnTableViewController : UITableViewController
+
+@end
