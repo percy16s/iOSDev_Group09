4933062f7e9a75e9f4a173a937d0ab25c87586c7
frantisan <frantisaul@gmail.com>
2014-01-30 20:58:47 -0500

    Creando SegundaClaseViewController.h

---
+++ module_1/class_03/CreacionProtocoloDelegado/CreacionProtocoloDelegado/SegundaClaseViewController.h
@@ -0,0 +1,13 @@
+//
+//  SegundaClaseViewController.h
+//  CreacionProtocoloDelegado
+//
+//  Created by Franti Saúl Huamán Mera on 1/30/14.
+//  Copyright (c) 2014 Franti Saúl Huamán Mera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SegundaClaseViewController : UIViewController
+
+@end
