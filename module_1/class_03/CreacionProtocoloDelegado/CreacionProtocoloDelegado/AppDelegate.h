6a1b2bbfd990b2d1a6ea04d3765767c8cb806d60
frantisan <frantisaul@gmail.com>
2014-01-30 20:11:39 -0500

    Initial Commit

---
+++ module_1/class_03/CreacionProtocoloDelegado/CreacionProtocoloDelegado/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CreacionProtocoloDelegado
+//
+//  Created by Franti Saúl Huamán Mera on 1/30/14.
+//  Copyright (c) 2014 Franti Saúl Huamán Mera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ module_1/class_03/CreacionProtocoloDelegado/CreacionProtocoloDelegado/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  CreacionProtocoloDelegado
+//
+//  Created by Franti Saúl Huamán Mera on 1/30/14.
+//  Copyright (c) 2014 Franti Saúl Huamán Mera. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
